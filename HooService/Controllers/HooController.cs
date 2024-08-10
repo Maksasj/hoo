d73cd14b3220af55f569c600863ba592f5005dc3
Maksim Jaroslavcevas <36482401+Maksasj@users.noreply.github.com>
2024-08-10 18:48:11 +0300

    microsoft graph api

--- HooService/Controllers/HooController.cs
+++ HooService/Controllers/HooController.cs
@@ -1,4 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Azure.Identity;
+using Azure.Core;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
 
 namespace HooService.Controllers
 {
@@ -13,10 +18,37 @@ namespace HooService.Controllers
             _logger = logger;
         }
 
+        private GraphServiceClient CreateGraphClient(string tenantId, string clientId)
+        {
+            string[] scopes = { "User.Read" };
+
+            var options = new InteractiveBrowserCredentialOptions()
+            {
+                ClientId = clientId,
+                TenantId = tenantId
+            };
+
+            var credential = new InteractiveBrowserCredential(options);
+            
+            return new GraphServiceClient(credential, scopes);
+        }
+
         [HttpGet]
         public async Task Get()
         {
+            var client = CreateGraphClient("****", "****");
 
+            _logger.LogInformation(client.ToString());
+
+            var count = await client.Applications.Count.GetAsync();
+
+            _logger.LogInformation(count.ToString());
+            /*
+            var userDriveId = drive.Id;
+
+            var rootItem = await client.Drives[userDriveId].Root.GetAsync();
+            _logger.LogInformation(rootItem.ToString());
+            */
         }
     }
 }
